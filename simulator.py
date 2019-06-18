d0bdd10c2e34650a6f0b305232fc7f7e3bf3b1eb
ThrawnCA <shell_layer-github@yahoo.com.au>
2019-06-18 13:14:05 +1000

    refactor
    - extract functions for each encounter type
    - drop special handling for keys and doors, just make them simple encounters
    - create global Hero object to hold stats and inventory
    - add error message for failed encounter lookup
    - use encounter table to get base stats for altar visits
    - add support for increasing life at altar (although optimal play won't use it)

--- simulator.py
+++ simulator.py
@@ -4,23 +4,112 @@ with open('encounters.json') as json_file:
     encounters = json.load(json_file)
 
 DEBUG = False
-stats = {}
-keys = {}
-inventory = {}
-altar_uses = 0
+
+class Hero:
+    stats = {}
+    keys = {}
+    inventory = {}
+    altar_uses = 0
+
+hero = Hero()
 
 def debug(message):
     if DEBUG:
         print "DEBUG: " + message
 
 def print_stats():
-    print "Life={},offense={},defence={},gold={},keys={},blue keys={},red keys={},inventory={}".format(stats['life'], stats['offense'], stats['defense'], stats['gold'], keys['yellow'], keys['blue'], keys['red'], inventory)
+    print "Stats={},keys={},inventory={}".format(hero.stats, hero.keys, hero.inventory)
 
-def altar_cost(altar_uses):
-    if altar_uses == 0:
-        return 20
+def simple_encounter(encounter, args):
+    if 'levelled' in encounter and encounter['levelled']:
+        multiplier = int(args[1])
     else:
-        return altar_cost(altar_uses - 1) + (altar_uses * 20)
+        multiplier = 1
+    for stat in ['offense', 'defense', 'life', 'gold']:
+        if stat in encounter:
+            hero.stats[stat] += encounter[stat] * multiplier
+            if hero.stats[stat] < 0:
+                raise Exception("Insufficient {stat}!".format(stat=stat))
+    if 'keys' in encounter:
+        for color in encounter['keys']:
+            hero.keys[color] += encounter['keys'][color]
+    if 'inventory' in encounter:
+        for item in encounter['inventory']:
+            if item in hero.inventory:
+                hero.inventory[item] += encounter['inventory'][item]
+            else:
+                hero.inventory[item] = encounter['inventory'][item]
+            if hero.inventory[item] < 0:
+                raise Exception("Not enough uses of {}".format(item))
+
+def monster_encounter(encounter, args):
+    if encounter['defense'] >= hero.stats['offense']:
+        raise Exception("Not enough offensive power to attack this monster!")
+    monster_life = encounter['life']
+    in_battle = True
+    while in_battle:
+        monster_life -= max(hero.stats['offense'] - encounter['defense'], 0)
+        if monster_life <= 0:
+            monster_life = 0
+            in_battle = False
+        if in_battle:
+            hero.stats['life'] -= max(encounter['offense'] - hero.stats['defense'], 0)
+            if hero.stats['life'] <= 0:
+                hero.stats['life'] = 0
+                raise Exception("Died!")
+    hero.stats['gold'] += encounter['gold']
+
+def altar_encounter(encounter, args):
+    if len(args) < 3:
+        raise Exception("Usage: altar <multiplier> <stat>")
+    """
+    Altar cost starts at 20 and increases with each usage.
+    The increment increases by 20 each time.
+    Thus, the cost is 20, then 40, 80, 140, 220, 320, 440, etc.
+
+    Altar effectiveness depends on the floor.
+    An altar in floors 1-10 gives 2 attack or 4 defence,
+    an altar in floors 11-20 gives 4 attack or 8 defence, etc.
+    """
+    def altar_cost(altar_uses):
+        if altar_uses == 0:
+            return 20
+        else:
+            return altar_cost(altar_uses - 1) + (altar_uses * 20)
+
+    cost = altar_cost(hero.altar_uses)
+    debug("Altar will cost {} gold.".format(cost))
+    if hero.stats['gold'] >= cost:
+        hero.stats['gold'] -= cost
+        hero.altar_uses += 1
+    else:
+        raise Exception("Not enough gold to use altar!")
+    multiplier = int(args[1])
+    stat = args[2]
+    if stat == 'offense':
+        hero.stats[stat] += encounter['offense'] * multiplier
+    elif stat == 'defense':
+        hero.stats[stat] += encounter['defense'] * multiplier
+    elif stat == 'life':
+        hero.stats[stat] += encounter['life'] * multiplier
+    else:
+        raise Exception("Unrecognised altar upgrade: {}, must be offense/defense/life".format(stat))
+
+def do_encounter(args):
+    encounter_name = args[0]
+    if encounter_name in encounters:
+        encounter = encounters[encounter_name]
+        debug("Found {}: {}".format(encounter_name, encounter))
+        if encounter['type'] == 'simple':
+            simple_encounter(encounter, args)
+        elif encounter['type'] == 'monster':
+            monster_encounter(encounter, args)
+        elif encounter['type'] == 'altar':
+            altar_encounter(encounter, args)
+        else:
+            raise Exception("Not implemented: {}".format(encounter['type']))
+    else:
+        raise Exception("Unrecognised encounter: {}".format(encounter_name))
 
 if len(sys.argv) > 1:
     simulation_file = sys.argv[1]
@@ -30,22 +119,22 @@ simulation = open(simulation_file)
 line = simulation.readline().strip()
 if line.startswith('Start'):
     stats_string = line.split(' ')[1].split('/')
-    stats['life'] = int(stats_string[0])
-    stats['offense'] = int(stats_string[1])
-    stats['defense'] = int(stats_string[2])
-    stats['gold'] = int(stats_string[3])
-    keys['yellow'] = int(stats_string[4])
-    keys['blue'] = int(stats_string[5])
-    keys['red'] = int(stats_string[6])
+    hero.stats['life'] = int(stats_string[0])
+    hero.stats['offense'] = int(stats_string[1])
+    hero.stats['defense'] = int(stats_string[2])
+    hero.stats['gold'] = int(stats_string[3])
+    hero.keys['yellow'] = int(stats_string[4])
+    hero.keys['blue'] = int(stats_string[5])
+    hero.keys['red'] = int(stats_string[6])
     line = simulation.readline().strip()
 else:
-    stats['life'] = 1000
-    stats['offense'] = 100
-    stats['defense'] = 100
-    stats['gold'] = 0
-    keys['yellow'] = 0
-    keys['blue'] = 0
-    keys['red'] = 0
+    hero.stats['life'] = 1000
+    hero.stats['offense'] = 100
+    hero.stats['defense'] = 100
+    hero.stats['gold'] = 0
+    hero.keys['yellow'] = 0
+    hero.keys['blue'] = 0
+    hero.keys['red'] = 0
 
 while line:
     if line.startswith('#'):
@@ -54,84 +143,6 @@ while line:
     elif line == 'print':
         print_stats()
     else:
-        args = line.split()
-        if args[0] == 'key':
-            color = line.split(' ')[1]
-            keys[color] += 1
-            debug("Found a " + color + " key.")
-        elif args[0] == 'door':
-            color = line.split(' ')[1]
-            if keys[color] > 0:
-                keys[color] -= 1
-                debug("Used a " + color + " key.")
-            else:
-                raise Exception("Not enough " + color + " keys!")
-        elif args[0] == 'altar':
-            """
-            Altar cost starts at 20 and increases with each usage.
-            The increment increases by 20 each time.
-            Thus, the cost is 20, then 40, 80, 140, 220, 320, 440, etc.
-
-            Altar effectiveness depends on the floor.
-            An altar in floors 1-10 gives 2 attack or 4 defence,
-            an altar in floors 11-20 gives 4 attack or 8 defence, etc.
-            """
-            cost = altar_cost(altar_uses)
-            debug("Altar will cost {} gold.".format(cost))
-            if stats['gold'] >= cost:
-                stats['gold'] -= cost
-                altar_uses += 1
-            else:
-                raise Exception("Not enough gold to use altar!")
-            multiplier = int(args[1])
-            stat = args[2]
-            if stat == 'offense':
-                stats[stat] += 2 * multiplier
-            elif stat == 'defense':
-                stats[stat] += 4 * multiplier
-            else:
-                raise Exception("Can only raise offense and defense at altars - if you're raising health, you're doing it wrong")
-        else:
-            encounter = encounters[args[0]]
-            debug("Found " + line + ": " + str(encounter))
-            if encounter['type'] == 'simple':
-                if 'levelled' in encounter and encounter['levelled']:
-                    multiplier = int(args[1])
-                else:
-                    multiplier = 1
-                for stat in ['offense', 'defense', 'life', 'gold']:
-                    if stat in encounter:
-                        stats[stat] += encounter[stat] * multiplier
-                        if stats[stat] < 0:
-                            raise Exception("Insufficient {stat}!".format(stat=stat))
-                if 'keys' in encounter:
-                    for color in encounter['keys']:
-                        keys[color] += encounter['keys'][color]
-                if 'inventory' in encounter:
-                    for item in encounter['inventory']:
-                        if item in inventory:
-                            inventory[item] += encounter['inventory'][item]
-                        else:
-                            inventory[item] = encounter['inventory'][item]
-                        if inventory[item] < 0:
-                            raise Exception("Not enough uses of {}".format(item))
-            elif encounter['type'] == 'monster':
-                if encounter['defense'] >= stats['offense']:
-                    raise Exception("Not enough offensive power to attack this monster!")
-                monster_life = encounter['life']
-                in_battle = True
-                while in_battle:
-                    monster_life -= max(stats['offense'] - encounter['defense'], 0)
-                    if monster_life <= 0:
-                        monster_life = 0
-                        in_battle = False
-                    if in_battle:
-                        stats['life'] -= max(encounter['offense'] - stats['defense'], 0)
-                        if stats['life'] <= 0:
-                            stats['life'] = 0
-                            raise Exception("Died!")
-                stats['gold'] += encounter['gold']
-            else:
-                raise Exception("Not implemented: " + encounter['type'])
+        do_encounter(line.split())
     line = simulation.readline().strip()
 
